be7a2b97714a5313b048277e266d265f9b29be0a
James Goodman <jamesgoodman206@gmail.com>
2022-10-25 12:39:28 +0100

    added hunger and fitness as well as tests to ensure that the pet gets hungry and unfit when aged with the growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,6 +14,16 @@ describe('constructor', () => {
   
       expect(pet.age).toEqual(0);
     });
+    it('has a initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.hunger).toEqual(0);
+    });
+    it('has a initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.fitness).toEqual(10);
+    });
   });
 
   describe('growUp', () => {
@@ -23,5 +33,19 @@ describe('constructor', () => {
       pet.growUp();
   
       expect(pet.age).toEqual(1);
+    });
+    it('increments the hunger by 5', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.hunger).toEqual(5);
+    });
+    it('decreases the fitness by 3', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.fitness).toEqual(7);
     });
   });
